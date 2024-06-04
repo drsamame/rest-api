a46a753031b881255f8a35c75d451046d91dc005
Diego Rodrigo Samame Salazar <tsm_dsamame@niubiz.com.pe>
2024-06-04 13:54:20 -0500

    api rest primera version

---
+++ schema/movie.js
@@ -0,0 +1,35 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+    title: z.string({
+      title: z.string({
+        invalid_type_error: 'Movie title must be a string',
+        required_error: 'Movie title is required',
+      })
+    }),
+    director: z.string(),
+    year: z.number().int().min(1900).max(2000),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10).optional(),
+    poster: z.string(),
+    genre: z.array(
+      z.enum(['Action', 'Adventure', 'Terror']),
+      {
+        invalid_type_error: 'Movie genre must be an array of strings',
+        required_error: 'Movie genre is required',
+      }
+    )
+  })
+
+function validateMovie(object){
+    return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie(input){
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+    validateMovie,
+    validatePartialMovie
+}
